e7e6ad8beffdca58433f46e1734bdb39187bd247
pawan779 <pawan.dharel77@gmail.com>
2020-01-04 16:54:32 +0545

    connected to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const dotenv = require('dotenv').config();
 const app=express();
 
 app.use(express.static(__dirname + "/public"));
 
+mongoose.connect(process.env.URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false,
+    useCreateIndex:true
+})
+.then((db)=>{
+    console.log("Sucessfully connected to MongoDB server");
+},(err)=>console.log(err));
+
 app.listen(process.env.PORT,() =>{
     console.log(`App is running at localhost:${process.env.PORT}`);
 })
+
