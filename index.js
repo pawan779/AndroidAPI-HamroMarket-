a9b53dd764180c4841cac5cdd6dfc7e39891dfed
pawan779 <pawan.dharel77@gmail.com>
2020-01-04 16:02:31 +0545

    index.html created

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express=require("express");
 const dotenv = require('dotenv').config();
-
-
 const app=express();
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(process.env.PORT,() =>{
     console.log(`App is running at localhost:${process.env.PORT}`);
