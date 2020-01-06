ef1067f708d1c865be3d3add8bacda48c1392c22
pawan779 <pawan.dharel77@gmail.com>
2020-01-06 12:12:44 +0545

    login created

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const morgan = require('morgan');
 const user=require("./models/users")
 const dotenv = require('dotenv').config();
 const userRouter=require('./routes/users');
-const app=express();
+const uploadRouter=require('./routes/upload');
 
+
+
+const app=express();
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
 mongoose.connect(process.env.URL,{
@@ -20,6 +25,13 @@ mongoose.connect(process.env.URL,{
 
 
 app.use('/users',userRouter);
+app.use('/upload',uploadRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.statusCode = 500;
+    res.json({ status: err.message });
+});
 
 app.listen(process.env.PORT,() =>{
     console.log(`App is running at localhost:${process.env.PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,66 @@
-const express=require('express');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
-const User=require('../models/users');
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+const router = express.Router();
 
-const router=express.Router();
-
-router.post('/signup',(req,res,next)=>{
-    let password=req.body.password;
-    bcrypt.hash(password,10,(err,hash)=>
-    {
-        if(err){
-            let err=new Error("Couldnot hash!");
-            err.status=500;
+router.post('/signup', (req, res, next) => {
+    let password = req.body.password;
+    bcrypt.hash(password, 10, function (err, hash) {
+        if (err) {
+            let err = new Error('Could not hash!');
+            err.status = 500;
             return next(err);
         }
-
         User.create({
-            firstName:req.body.firstName,
-            lastName:req.body.lastName,
-            address:req.body.address,
-            phone:req.body.phone,
-            username:req.body.username,
-            password:hash,
-            image:req.body.image
-        })
-        .then((user)=>{
-            let token=jwt.sign({_id:user_id},
-                process.env.SECRET);
-                res.json({status:"Signup sucessfully!",
-                token:token});
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            address: req.body.address,
+            phone: req.body.phone,
+            username: req.body.username,
+            password: hash,
+            image: req.body.image
+        }).then((user) => {
+            let token = jwt.sign({
+                _id: user._id
+            }, "this is secret key");
+            res.json({
+                status: "Signup success!",
+                token: token
+            });
         }).catch(next);
     });
 });
 
-module.exports=router;
+router.post('/login', (req, res, next) => {
+    User.findOne({
+            username: req.body.username
+        })
+        .then((user) => {
+            if (user == null) {
+                let err = new Error("User not found!");
+                err.status = 401;
+                return next(err);
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then((isMatch) => {
+                        if (!isMatch) {
+                            let err = new Error('Password does not match!');
+                            err.status = 401;
+                            return next(err);
+                        }
+                        let token = jwt.sign({
+                            _id: user._id
+                        }, process.env.SECRET);
+                        res.json({
+                            status: 'Login success!',
+                            token: token
+                        });
+                    }).catch(next);
+            }
+
+        }).catch(next);
+})
+
+
+module.exports = router;
