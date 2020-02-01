b24fba5922567016989514e7d0455be93856de3b
pawan779 <pawan.dharel77@gmail.com>
2020-02-01 12:46:26 +0545

    admin get user product by id

--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,12 @@ const Product = require('../models/products');
 const router = express.Router();
 const auth = require('../auth');
 
+router.get("/product/:id",auth.verifyAdmin,(req,res,next)=>{
+    Product.findById({_id:req.params.id})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch(next)
+})
 
 module.exports=router;
